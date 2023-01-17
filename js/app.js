b1851b1c943df8ba67364b81d0552fbd754f12bf
DmitryRad <server@internet.ru>
2023-01-17 23:15:29 +0300

    project files added

---
+++ js/app.js
@@ -0,0 +1,10 @@
+new Swiper('.slider', {
+    // freeMode: true,
+    speed: 2400,
+    mousewheel: {
+        enabled: true,
+        sensitivity: 2.4
+    },
+    spaceBetween: 18,
+    parallax: true
+})
